afe398a2a42a78ce7996dc1f3c6f6ef2d1ce9942
benmp <benmpbusiness@gmail.com>
2017-03-14 21:57:15 -0500

    page api for top 10(00) live channels and connect to their irc

--- Program.cs
+++ Program.cs
@@ -5,27 +5,30 @@ using System.Threading.Tasks;
 using System.Net.Http;
 using Newtonsoft.Json;
 using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections.Concurrent;
 
 namespace ConsoleApplication
 {
     public class Program
     {
         //TODO
-        //Read in all important information like keys from config file (ini)
         //Get top 1000 channels by viewercount by name
+        //Put 1000 names somewhere useful
         //Connect to all 1000 chat channels
         //Identify all emotes
         //Accrue emote counts per minute
         //Print table of emotes (basic)
         //Handle channel streaming ending
         //Handle top 1000 changing (maybe streams with > 1000 viewers at any point?)
+        static ConcurrentDictionary<long, string> concurrentDictionary = new ConcurrentDictionary<long, string>();
         public static void Main(string[] args)
         {
             //Essentially the same thing on a console app
             //Task.WaitAll(GetAPI(), ConnectIRC());
-            Task.WhenAll(GetAPI(), 
-                         ConnectIRC())
-                         .GetAwaiter().GetResult();
+            Task.WhenAll(GetAPI()).GetAwaiter().GetResult();
+            Task.WhenAll(ConnectIRC()).GetAwaiter().GetResult();
 
             Console.WriteLine("Complete");
         }
@@ -45,15 +48,29 @@ namespace ConsoleApplication
 
                 httpClient.DefaultRequestHeaders.Add("Accept", configuration["twitchapi:headersaccept"]);
                 httpClient.DefaultRequestHeaders.Add("Client-ID", configuration["twitchapi:headersclientid"]);
-
-                string requestURL = "/kraken/streams";
-                HttpResponseMessage httpResponseMessage = await httpClient.GetAsync(requestURL);
-                string response = await httpResponseMessage.Content.ReadAsStringAsync();
-                dynamic jsonResponse = JsonConvert.DeserializeObject(response);
-                foreach (dynamic stream in jsonResponse["streams"])
+                short[] repeat = new short[1/*0*/] { 0/*, 99, 199, 299, 399, 499, 599, 699, 799, 899*/}; // Spare API for not until rate limiting in place
+                foreach (short offset in repeat)
                 {
-                    Console.WriteLine($"Game: {stream["game"],-40} Viewers: {stream["viewers"],-10} Channel: {stream["channel"]["display_name"]} ");
+                    string response = await RequestURL("/kraken/streams/", new Dictionary<string, string>() { { "limit", "10"/*100*/ }, { "offset", offset.ToString() } }, httpClient);
+                    PrintResponse(response, offset);
                 }
+            }
+        }
+
+        public static async Task<string> RequestURL(string url, Dictionary<string, string> queryString, HttpClient httpClient)
+        {
+            string requestURL = string.Format("{0}{1}", url, ToQueryString(queryString));
+            HttpResponseMessage httpResponseMessage = await httpClient.GetAsync(requestURL);
+            return await httpResponseMessage.Content.ReadAsStringAsync();
+        }
+
+        public static void PrintResponse(string response, short rank)
+        {
+            dynamic jsonResponse = JsonConvert.DeserializeObject(response);
+            foreach (dynamic stream in jsonResponse["streams"])
+            {
+                Console.WriteLine($"Rank: {++rank, -5} Game: {stream["game"],-50} Viewers: {stream["viewers"],-7} Channel: {stream["channel"]["display_name"]} ");
+                concurrentDictionary[(long)stream["channel"]["_id"]] = (string)stream["channel"]["name"];
             }
         }
 
@@ -76,7 +93,12 @@ namespace ConsoleApplication
                         streamWriter.AutoFlush = true;
                         await streamWriter.WriteLineAsync($"PASS oauth:{configuration["twitchirc:oauth"]}");
                         await streamWriter.WriteLineAsync($"NICK {configuration["twitchirc:nick"]}");
-                        await streamWriter.WriteLineAsync("JOIN #twitchpresents");
+
+                        foreach (string channelName in concurrentDictionary.Values)
+                        {
+                            await streamWriter.WriteLineAsync($"JOIN #{channelName}");
+                        }
+
                         while (true)
                         {
                             string readLine = await streamReader.ReadLineAsync();
@@ -89,6 +111,11 @@ namespace ConsoleApplication
                     }
                 }
             }
+        }
+
+        public static string ToQueryString(Dictionary<string, string> source)
+        {
+            return string.Format("?{0}", String.Join("&", source.Select(kvp => String.Format("{0}={1}", kvp.Key, kvp.Value))));
         }
     }
 }
